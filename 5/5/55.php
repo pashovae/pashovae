dc3315bb0c6e6eee485efc6cf6b37fbc21831ae5
pashovae <pashovae@gmail.com>
2014-11-13 20:05:36 +0300

    55

---
+++ 5/5/55.php
@@ -0,0 +1,25 @@
+
+<?php 
+  if(isset($_GET['ident'])){
+    $ident=$_GET['ident'];
+  }
+  else{
+    die('введите параметр');
+  }
+  
+  if(empty($ident)){
+    die('пустая строка');
+  }
+  
+  if(!ctype_alpha($ident[0])){
+    die('Последовательность не идентификатор');
+  }
+  
+  for($i=1; $i<strlen($ident); $i++){
+    if(!ctype_alnum($ident[i])){
+       die('no');
+    }
+  }
+  echo('yes');
+
+?>
