357dd484e740a2ecdf1ee36080bdbeae1c74fcbe
pashovae <pashovae@gmail.com>
2014-11-13 20:21:53 +0300

    форматирование

--- 5/5/55.php
+++ 5/5/55.php
@@ -1,24 +1,29 @@
 ﻿<?php
-  if(isset($_GET['ident'])){
-    $ident=$_GET['ident'];
-  }
-  else{
-    die('введите параметр');
-  }
-  
-  if(empty($ident)){
-    die('пустая строка');
-  }
-  
-  if(!ctype_alpha($ident[0])){
-    die('Последовательность не идентификатор');
-  }
-  
-  for($i=1; $i<strlen($ident); $i++){
-    if(!ctype_alnum($ident[i])){
-       die('no');
+    if(isset($_GET['ident']))
+	{
+        $ident=$_GET['ident'];
     }
-  }
-  echo('yes');
-
+    else
+	{
+        die('введите параметр');
+    }
+  
+    if(empty($ident))
+    {
+        die('пустая строка');
+    }
+  
+    if(!ctype_alpha($ident[0]))
+    {
+        die('Последовательность не идентификатор');
+    }
+  
+    for($i=1; $i<strlen($ident); $i++)
+    {
+        if(!ctype_alnum($ident[i]))
+	    {
+            die('no');
+        }
+    }
+    echo('yes');
 ?>
